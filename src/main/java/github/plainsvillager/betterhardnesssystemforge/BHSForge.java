a315ca8fe5c855546df70aa59eb7aef9baa6f8b2
PlainsVillager <littlecuckoo@outlook.com>
2023-01-28 10:51:19 +0800

    rebase injected

---
+++ src/main/java/github/plainsvillager/betterhardnesssystemforge/BHSForge.java
@@ -0,0 +1,61 @@
+package github.plainsvillager.betterhardnesssystemforge;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+@Mod(BHSForge.MODID)
+@Mod.EventBusSubscriber(modid = BHSForge.MODID)
+public class BHSForge {
+
+    public static final String MODID = "bhs_forge";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BHSForge() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+        LOGGER.info("这个傻逼Logger能干什么啊");
+    }
+
+    @SubscribeEvent
+    private static void addCreative(CreativeModeTabEvent.BuildContents event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
---
+++ src/main/java/github/plainsvillager/betterhardnesssystemforge/class_114514.java
@@ -0,0 +1,18 @@
+package github.plainsvillager.betterhardnesssystemforge;
+
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class class_114514 {
+    public static final int BASE = 0;
+    public static final int MAX = 114514;
+
+
+    @SubscribeEvent
+    public static void rightClickBlock(PlayerInteractEvent.RightClickBlock event){
+
+    }
+
+}
